4d988afd49f70eff03bac15e632ced7e0c64bada
Quynh T Vu <Quynh.T.Vu-1@ou.edu>
2020-09-07 20:41:40 -0500

    Added an example with student UML with exception handling

---
+++ ReviewProblems/StudentUMLExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StudentUMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.FirstName = "Adam";
+            myStudent.LastName = "Ackerman";
+            myStudent.IsOnProbation = true;
+            myStudent.SoonerID = 5;
+
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+            myStudent.MakePayment(50);
+            Console.WriteLine(myStudent);
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            try
+            {
+                myStudent.MakePayment(-500);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("INVALID PAYMENT");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReviewProblems/StudentUMLExample/Student.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentUMLExample
+{
+    class Student
+    { 
+        //Write on multiple lines
+        //Alt+Shift+Arrow keys to select which lines you want to edit and then release keys and time
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance;
+
+        //Default/Empty Constructor for the Student
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 1000;
+        }
+
+        //ctor+2TABS
+        /// <summary>
+        /// Overloaded constructor that will set the default values of soonerid, firstname, lastname, and bursar balance to existing values
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fName"></param>
+        /// <param name="lName"></param>
+        /// <param name="BursarBalance"></param>
+        public Student(int id, string fName, string lName, double BursarBalance) : this()
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            //IsOnProbation = false;
+            //GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        /// <summary>
+        /// Make a payment on your bursar balance
+        /// </summary>
+        /// <param name="amount">A non-negative value to reduce your bursar balance by</param>
+        public void MakePayment(double amount)
+        {
+            //BursarBalance = BursarBalance - amount;
+            if (amount > 0)
+            {
+                BursarBalance -= amount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        /// <summary>
+        /// The current state of your bursar balance
+        /// </summary>
+        /// <returns>  How much you owe the Bursar</returns>
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        /// <summary>
+        /// Create a string representation of our instance of the class
+        /// </summary>
+        /// <returns>The current state of the object</returns>
+        public override string ToString()
+        {
+            string output = $"{FirstName} {LastName} ({SoonerID})";
+            if (IsOnProbation == true)
+            {
+                output += " is on probation.\n";
+            }
+            else
+            {
+                output += " is not on probation.\n";
+            }
+            output += $"They have a {GPA.ToString("N2")} GPA + and they owe {BursarBalance.ToString("C2")}";
+            
+            return output;
+
+        }
+    }
+}
